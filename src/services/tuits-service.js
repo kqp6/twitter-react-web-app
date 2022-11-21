ca203f15bd453ba892dea7e2f0fd710c806bf78f
K <kqlykq@gmail.com>
2022-11-21 16:46:58 -0500

    update

--- src/services/tuits-service.js
+++ src/services/tuits-service.js
@@ -18,7 +18,7 @@ export const deleteTuit = async (tid) => {
   return response.data
 }
 export const updateTuit = async (tuit) => {
-    const response = await axios
+    await axios
     .put(`${TUITS_API}/${tuit._id}`, tuit);
     return tuit;
 }
