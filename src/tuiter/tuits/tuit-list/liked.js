7499372faa6a6b86870650d5168bbb3cf0576b0c
K <kqlykq@gmail.com>
2022-12-02 02:17:56 -0500

    a9 Mongo fa22 update

--- src/tuiter/tuits/tuit-list/liked.js
+++ src/tuiter/tuits/tuit-list/liked.js
@@ -1,10 +1,21 @@
 import React from "react";
+import {updateTuitThunk} from "../../../services/tuits-thunks";
+import {useDispatch} from "react-redux";
 
-const ConditionalLiked = ({liked}) => {
-    if (liked) {
-        return (<i className="bi bi-heart-fill text-danger"></i>);
+const ConditionalLiked = ({tuit}) => {
+    const dispatch = useDispatch();
+    if (tuit.liked) {
+        return (<i onClick={() => dispatch(updateTuitThunk({
+            ...tuit,
+            likes: tuit.likes - 1,
+            liked: false
+        }))} className={`bi me-2 ${tuit.liked ? "text-danger bi-heart-fill" : "bi-heart"}`}></i>);
     } else {
-        return (<i className="bi bi-heart "></i>);
+        return (<i onClick={() => dispatch(updateTuitThunk({
+            ...tuit,
+            likes: tuit.likes + 1,
+            liked: true
+        }))} className={`bi me-2 ${tuit.liked ? "text-danger bi-heart-fill" : "bi-heart"}`}></i>);
     }
 };
 
--- src/tuiter/tuits/tuit-list/tuit-stats.js
+++ src/tuiter/tuits/tuit-list/tuit-stats.js
@@ -1,11 +1,11 @@
 import React from "react";
-//import ConditionalLiked from "./liked";
+import ConditionalLiked from "./liked";
 import {updateTuitThunk} from "../../../services/tuits-thunks";
 import {useDispatch} from "react-redux";
 
 
 const TuitStats = ({tuit}) => {
-    const dispatch = useDispatch();
+    // const dispatch = useDispatch();
     return(
         <div className="row">
             <div className="col-3">
@@ -15,10 +15,7 @@ const TuitStats = ({tuit}) => {
                 <i className="bi bi-arrow-repeat"></i> {tuit.retuits}
             </div>
             <div className="col-3">
-                <i onClick={() => dispatch(updateTuitThunk({
-                    ...tuit,
-                    likes: tuit.likes + 1
-                }))} className="bi bi-heart-fill me-2 text-danger"></i> {tuit.likes}
+                <ConditionalLiked tuit = {tuit}/> {tuit.likes}
             </div>
             <div className="col-3">
                 <i className="bi bi-share"></i>
@@ -28,3 +25,10 @@ const TuitStats = ({tuit}) => {
 }
 
 export default TuitStats;
+
+/*
+<i onClick={() => dispatch(updateTuitThunk({
+    ...tuit,
+    likes: tuit.likes + 1
+}))} className="bi bi-heart-fill me-2 text-danger"></i>
+*/
